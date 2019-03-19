a79b4ee1255d44bba4ecc434ca48aff1ecc35641
Magalhaes <tiago.magalhaes@capgemini.com>
2019-03-19 17:40:10 -0300

    Clientes: back-end e front-end

--- src/main/java/com/projeto/x/boot/web/controller/ClienteController.java
+++ src/main/java/com/projeto/x/boot/web/controller/ClienteController.java
@@ -2,8 +2,10 @@ package com.projeto.x.boot.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -32,16 +34,37 @@ public class ClienteController {
 		return "/cliente/cadastro";
 	}
 	
-	@GetMapping("/listar")
-	public String listar( ) {
-		return "/cliente/lista";
-	}
-	
 	@PostMapping("/salvar")
 	public String salvar(Cliente cliente, RedirectAttributes attr) {
 		clienteService.salvar(cliente);
 		attr.addFlashAttribute("success", "Cliente inserido com sucesso.");
 		return "redirect:/clientes/cadastrar";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("cliente", clienteService.buscarPorId(id));
+		return "cliente/cadastro";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Cliente cliente, RedirectAttributes attr) {
+		clienteService.editar(cliente);
+		attr.addFlashAttribute("success", "Cliente editado com sucesso.");
+		return "redirect:/clientes/listar";
+	}	
+	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, RedirectAttributes attr) {
+		clienteService.excluir(id);
+		attr.addFlashAttribute("success", "Cliente excluido com sucesso.");
+		return "redirect:/clientes/listar"; 	
+	}
+	
+	@GetMapping("/listar")
+	public String listar(ModelMap model ) {
+		model.addAttribute("clientes", clienteService.buscarTodos());
+		return "/cliente/lista";
 	}
 	
 	@ModelAttribute("ufs")
